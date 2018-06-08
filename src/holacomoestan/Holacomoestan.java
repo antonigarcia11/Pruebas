f4468373de29f38e57530ff63353a1240cdce57a
Antoni Garcia <garciamt2796@gmail.com>
2018-06-08 10:32:25 -0600

    Correcciones hechas

--- src/holacomoestan/Holacomoestan.java
+++ src/holacomoestan/Holacomoestan.java
@@ -18,15 +18,15 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Holacomoestan extends Application {    
-    //Escenario principal
+    
     private Stage escenarioPrincipal;
     private final String PAQUETE_VISTA = "/com/holacomoestan/view/";
     @Override
-    //Metodo que iniciar la carga del escenario principal
+    
     public void start(Stage escenarioPrincipal) {
         this.escenarioPrincipal = escenarioPrincipal;
         this.escenarioPrincipal.setTitle("andres");
-        //mostrarHelloWorld();
+      
         mostrarLogin();
         this.escenarioPrincipal.show();
     }
@@ -39,24 +39,24 @@ public class Holacomoestan extends Application {
         }
     }
     
-    //Metodo para cambiar el escenario principal
+    
     public Initializable cambiarEscena(String fxml, int ancho, int alto) throws IOException{
         Initializable resultado = null;
-        //Cargador del archivo FXML
+        
         FXMLLoader cargadorFXML = new FXMLLoader();
-        //Asignacion del archivo logico
+        
         InputStream archivo = Holacomoestan.class.getResourceAsStream(PAQUETE_VISTA + fxml);
-        //Cargdor de fabrica para cargar el archivo FXML
+        
         cargadorFXML.setBuilderFactory(new JavaFXBuilderFactory());
-        //Direccion de la ruta del archivo FXML
+        
         cargadorFXML.setLocation(Holacomoestan.class.getResource(PAQUETE_VISTA + fxml));        
-        //Creacion de la escena
+        
         Scene escena = new Scene((AnchorPane)cargadorFXML.load(archivo),ancho,alto); 
-        //Asignacion de la escena en el escenario principal
+       
         this.escenarioPrincipal.setScene(escena);
-        //Ajustar el tamaño del escenario a la escena
+        
         this.escenarioPrincipal.sizeToScene();
-        //Retornar el objeto initializable del controlador
+       
         resultado = (Initializable)cargadorFXML.getController();
         return resultado;
     }
